3e79d9142a1adc889e68900e5a82301c457c8b3c
SemperSpirit <kaiten-aiko@mail.ru>
2023-12-21 11:50:52 +0300

    deleted-all-comments

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  Test
-//
-//  Created by Анастасия Шалагинова on 21/12/23.
-//
+
 
 import UIKit
 
@@ -11,7 +6,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     func setupView() {
         view.backgroundColor = .systemRed
