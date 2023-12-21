a28853ef247373bc9961cd83eb5e26220f910e91
SemperSpirit <kaiten-aiko@mail.ru>
2023-12-21 11:03:12 +0300

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Анастасия Шалагинова on 21/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
